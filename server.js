681fe7ea952ea7546093972a5a3db56f371773b1
WinnMBG <winn77.m@hotmail.com>
2024-03-22 13:56:13 +0100

    pushed

--- server.js
+++ server.js
@@ -50,6 +50,6 @@ app.post('/data', async (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
+app.listen(port,'0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
